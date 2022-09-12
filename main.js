343553861028c32886ca7c9bb176304f26ae1cd6
Ezequiel Lessa de oliveira <ezequiellessa@gmail.com>
2022-09-11 21:05:32 -0300

    menu reponsivo

---
+++ main.js
@@ -0,0 +1,14 @@
+
+
+
+
+
+function menuShow(){
+    var container = document.querySelector('.mobile-menu');
+    
+  if(container.style.display === 'block') {
+      container.style.display = 'none';
+  } else {
+      container.style.display = 'block';
+  }
+};
